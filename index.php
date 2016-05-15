648c47828b973399089cb38fa91d6ef8c50495ee
trigger <trigger.professional@gmail.com>
2016-05-15 17:24:40 +0100

    Iteration 1 - Home page with simple MVC

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// @todo - Integrate Silex framework
+// @todo - Reorganise the source code
+// @todo - Implement frontal controler
 require 'modele.php';
 $books = getBooks();
 require 'view.php';
